14511a634c0b7bde313befab949e9fe52fa24fe6
merrillmckee <merrillmckee@gmail.com>
2025-01-09 11:58:58 -0500

    read in an image

--- src/__tests__/test_file_utils.py
+++ src/__tests__/test_file_utils.py
@@ -1,12 +1,12 @@
 from pathlib import Path
+from src.file_utils import read_image
 
 
-def run_model():
-    pass
+def test_read_image():
 
-
-if __name__ == "__main__":
     data_path = Path('/Users/merrillmck/source/github/roof_modeling/data')
     uid = "ftlaud_1"
     img = read_image(data_path, uid)
-    run_model()
+
+    assert len(img.shape) == 3  # RGB
+    assert img.shape[2] == 3  # HxWx3
--- src/file_utils.py
+++ src/file_utils.py
@@ -1,5 +1,29 @@
 import numpy as np
+from pathlib import Path
+from PIL import Image
 
 
-def read_image(uid: str) -> np.ndarray:
-    pass
+# a "data" folder is organized with the following structure
+#
+# data/<uid>/ortho.png
+# data/<uid>/dsm.ply
+# data/<uid>/...
+#
+
+
+def read_image(data_path: Path, uid: str) -> np.ndarray:
+
+    path = Path(data_path) / uid / "ortho.png"
+
+    try:
+        p_img = Image.open(path)
+    except FileNotFoundError as e:
+        print(e)
+        return
+
+    img = np.array(p_img)
+
+    # cleanup
+    p_img.close()
+
+    return img
