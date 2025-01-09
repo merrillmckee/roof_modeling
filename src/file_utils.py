588fcba2beb41a714b91a0daf90108381976e70b
merrillmckee <merrillmckee@gmail.com>
2025-01-09 11:36:26 -0500

    initial files and folder structure

---
+++ src/file_utils.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def read_image(uid: str) -> np.ndarray:
+    pass
---
+++ src/run_model.py
@@ -0,0 +1,10 @@
+
+
+def run_model():
+    pass
+
+
+if __name__ == "__main__":
+    uid = "ftlaud_1"
+    img = read_image(uid)
+    run_model()
